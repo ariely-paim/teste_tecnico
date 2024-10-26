1e6e89d9f5b22ff82ff8faf2ca68332f74887c4d
Arielly Paim <112345620+ariely-paim@users.noreply.github.com>
2024-10-26 20:45:01 -0300

    Update codigo_teste2.c

--- codigo_teste2.c
+++ codigo_teste2.c
@@ -3,3 +3,28 @@
 IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 */
 
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int caracter = 0;
+    int cont = 0;
+
+    printf("Diga uma frase: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[caracter] != '\0') {
+        if (str[caracter] == 'A' || str[caracter] == 'a') {
+            cont++;
+        }
+        caracter++;
+    }
+
+    if (cont > 0) {
+        printf("A letra 'A' ou 'a' ocorre %d vez(es) na frase dada.\n", cont);
+    } else {
+        printf("A letra 'A' ou 'a' não ocorre na frase dada.\n");
+    }
+
+    return 0;
+}
