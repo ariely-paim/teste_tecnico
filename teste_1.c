98dd41e3eccc71007a09506ec92c588687feb4db
Arielly Paim <112345620+ariely-paim@users.noreply.github.com>
2024-10-26 20:06:16 -0300

    Update teste_1.c

--- teste_1.c
+++ teste_1.c
@@ -3,3 +3,33 @@
 IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
 */
 
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    int numero, c;
+
+    printf("Diga um número: ");
+    scanf("%d", &numero);
+
+    if (numero == a || numero == b) {
+        printf("O número %d pertence à sequência de Fibonacci.\n", numero);
+        return 0;
+    }
+
+    while (b <= numero) {
+        c = a + b;
+        if (c == numero) {
+            printf("O número %d pertence à sequência de Fibonacci.\n", numero);
+            return 0;
+        }
+        a = b;
+        b = c;
+    }
+
+    printf("O número %d não pertence à sequência de Fibonacci.\n", numero);
+
+    return 0;
+}
